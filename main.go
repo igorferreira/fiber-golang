310aee44884d62c5c75c943e88953e66c51a4122
igorferreira <igorferreirabr@gmail.com>
2020-12-27 19:25:34 -0300

    adicionado path hello

--- main.go
+++ main.go
@@ -7,35 +7,37 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-
 type healthStatus struct {
-    Status   string `json:"status"`
+	Status string `json:"status"`
 }
-
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 5")
+		return c.SendString("UP")
 	})
 
-	app.Get("/user/:name", func(c *fiber.Ctx) error  {
-		c.Set("Connection","keep-alive")
-		c.Set("Content-Type","text/html; charset=utf-8")
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 200000")
+	})
+
+	app.Get("/user/:name", func(c *fiber.Ctx) error {
+		c.Set("Connection", "keep-alive")
+		c.Set("Content-Type", "text/html; charset=utf-8")
 		resp := " Welcome to fiber, dear " + c.Params("name") + "!"
 		return c.SendString(resp)
 	})
 
 	app.Get("/health", func(c *fiber.Ctx) error {
-		c.Set("Content-type","application/json")
-		status := &healthStatus{ Status:   "OK"}
+		c.Set("Content-type", "application/json")
+		status := &healthStatus{Status: "OK"}
 		statusJSON, _ := json.Marshal(status)
 		fmt.Println(string(statusJSON))
 		return c.SendString(string(statusJSON))
 	})
 
-	app.Static("/app","./public")
+	app.Static("/app", "./public")
 
 	app.Listen(":8080")
 }
